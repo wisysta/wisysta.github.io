811dc0e63434a77ce0cac08509bb141394f9e6a8
wisysta <choi199042@gmail.com>
2023-09-07 22:04:14 +0900

    first commit

---
+++ precache-manifest.1d86a0ecef56e907469c6b8d4fbf4a6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41157b22ad48fef62c00d12ba8e35462",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d2b3aa4025faa5f910a",
+    "url": "/static/css/2.ebe76577.chunk.css"
+  },
+  {
+    "revision": "e5822f66fd5fe0cccedd",
+    "url": "/static/css/main.741e9f1a.chunk.css"
+  },
+  {
+    "revision": "3d2b3aa4025faa5f910a",
+    "url": "/static/js/2.3ee215e2.chunk.js"
+  },
+  {
+    "revision": "e5822f66fd5fe0cccedd",
+    "url": "/static/js/main.e5a0ce7e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
